ba522fef23f7eb252c0808c5a6332e282a54203e
Prasetya Aji <prasetya.aji@reservasi.com>
2018-10-15 14:38:46 +0700

    display books

--- graphql-playlist/client/src/components/BookList.js
+++ graphql-playlist/client/src/components/BookList.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { compose, graphql } from 'react-apollo';
+
+
+const getBooksQuery = gql`
+    {
+        books{
+            name
+            id
+        }
+    }
+`;
 
 class BookList extends Component {
-  render() {
-    return (
-      <div>
-        <ul id="book-list">
-            <li>Book name</li>
-        </ul>
-      </div>
-    );
-  }
+    displayBooks(){
+        const { loading, books } = this.props.data;
+
+        if(loading) {
+            return ( <div>Loading books...</div> );
+        } else {
+            return  books.map(book => {
+                return (
+                    <li key={book.id}>{book.name}</li>
+                )
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <ul id="book-list">
+                    { this.displayBooks() }
+                </ul>
+            </div>
+        );
+    }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
--- graphql-playlist/server/app.js
+++ graphql-playlist/server/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+// allow cross-origin request
+app.use(cors());
 
 // connect to mlab database
 mongoose.connect('mongodb://admin:admin123@ds131983.mlab.com:31983/gql-ninja');
